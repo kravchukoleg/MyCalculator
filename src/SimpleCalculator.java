84c74be4318dbfc96b730dc3052aaa04e9bf0fa7
kravchukoleg <kravchuk_@outlook.com>
2017-08-04 17:18:45 +0300

    new version

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -2,6 +2,43 @@
  * Created by kravchuk on 29.07.2017.
  */
 import javax.swing.*;
+import java.awt.FlowLayout;
 
 public class SimpleCalculator {
+
+    public static void main(String[] args){
+
+        JPanel windowsContent = new JPanel();
+
+        FlowLayout fl = new FlowLayout();
+        windowsContent.setLayout(fl);
+
+        JLabel label1 = new JLabel("Number 1:");
+        JTextField field1 = new JTextField(10);
+
+        JLabel label2 = new JLabel("Number 2:");
+        JTextField field2 = new JTextField(10);
+
+        JLabel label3 = new JLabel("Sum:");
+        JTextField result = new JTextField(10);
+
+        JButton go = new JButton("Add");
+
+        windowsContent.add(label1);
+        windowsContent.add(field1);
+        windowsContent.add(label2);
+        windowsContent.add(field2);
+        windowsContent.add(label3);
+        windowsContent.add(result);
+        windowsContent.add(go);
+
+        JFrame frame = new JFrame("My first calculator");
+        frame.setContentPane(windowsContent);
+
+        frame.setSize(400, 100);
+        frame.setVisible(true);
+
+    }
+
+
 }
--- src/SimpleCalculatorTest.groovy
+++
@@ -1,5 +0,0 @@
-/**
- * Created by kravchuk on 29.07.2017.
- */
-class SimpleCalculatorTest extends groovy.util.GroovyTestCase {
-}
